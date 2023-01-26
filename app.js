246f35fde80fa4e32d042dc9128d9de8e4ac0037
Braulio Díaz <braulio.diaz.gajardo@ciisa.cl>
2023-01-26 18:01:33 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express')
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('<h1><center><u> ¡Hello world from nodejs! </u></center></h1>')
+});
+
+app.listen(3000, function(req, res){
+    console.log('Server listening on port 3000')
+});
